aa71679576954a69f7f47d56f6f55d3618fd6bcc
Yornel <yornelmarval@gmail.com>
2026-04-20 20:39:55 -0400

    Update health.controller.ts

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
+import { Public } from '@/auth/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
   constructor(@InjectConnection() private readonly connection: Connection) {}
 
+  @Public()
   @Get()
   check(): { status: string; uptime: number; db: string; timestamp: string } {
     const dbStateMap: Record<number, string> = {
